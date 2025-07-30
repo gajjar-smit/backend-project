a77acec76b786380b07495f6a28b9937ccba576a
gajjar-smit <gajjarrudra24@gmail.com>
2025-07-30 12:54:11 +0530

    setup cloudinary utility and multer middleware files

--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
 <h1> this is chai aur code backend </h1>
+
+<ul>
+<li>Backend:- express</li>
+<li>Strorage:- Cloudinary</li>
+</ul>
+
+<!-- <h3> docs of librerys </h3> -->
---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudnary} from 'cloudinary'
+import fs from 'fs'
+
+  cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key:  process.env.CLOUDINARY_API_KEY, 
+        api_secret:  process.env.CLOUDINARY_API_SCERET 
+    });
+
+
+const uploadOnCloudinary =async (localFilePath)=>{
+
+    try {
+        if(!localFilePath) return null
+        //upload file on cloudnary
+        const response= await cloudnary.uploader.upload(localFilePath,{
+            resource_type:'auto'
+        })
+        //file had been uploaded successfull
+        console.log('file uploaded on cloudinary succcesfully ',response.url);
+        return response;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath)//remove mellecious local saved temp file if not found  local path in upload
+        return null;
+    }
+}
+export {uploadOnCloudinary}
